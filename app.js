a5644bf773bbf4934d77808721e5939d2bd2a45e
Jeff Ripke <jripke74@gmail.com>
2021-11-20 08:47:30 -0800

    add server

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const userName = 'Jeff';
+const http = require('http');
 
-console.log(userName);
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end('<h1>Hello There!!!</h1>');
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
