059ec1494d4ebcd6f9dbe16c432f35dcacdf78a4
jripke74@gmail.com <jripke74@gmail.com>
2021-11-22 11:32:54 -0800

    add post for recommend form

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", 'index.html');
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
   res.sendFile(htmlFilePath);
 });
 
@@ -16,18 +18,32 @@ app.get("/restaurants", function (req, res) {
   res.sendFile(htmlFilePath);
 });
 
-app.get('/recommend', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
 });
 
-app.get('/confirm', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
   res.sendFile(htmlFilePath);
 });
 
-app.get('/about', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
   res.sendFile(htmlFilePath);
 });
 
